07c0e3dcb4537626b5930abbf51acb2b1b7e153b
Michiel Proost <michiel.proost@gmail.com>
2020-10-05 10:50:45 +0200

    Created Unit Test

---
+++ Raytracing3DEngine/src/test/java/MatrixTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class MatrixTest {
+
+    @Test
+    void multiplication(){
+        int number1 = 5;
+        int number2 = 5;
+        assert(number1 == number2);
+    }
+
+}
