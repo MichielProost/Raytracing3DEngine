733a3c27d53e60b1efbb591c45f0996207e7a6f1
Michiel Proost <michiel.proost@gmail.com>
2020-10-05 10:19:45 +0200

    Add Project

---
+++ Raytracing3DEngine/src/com/github/MichielProost/Raytracing3DEngine/Raytracing3DEngine/DrawingComponent.java
@@ -0,0 +1,31 @@
+package com.github.MichielProost.Raytracing3DEngine.Raytracing3DEngine;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JComponent;
+
+public class DrawingComponent extends JComponent{
+
+    int xLoc;   // x location.
+    int yLoc;   // y location.
+
+    // Constructor.
+    public DrawingComponent(int x, int y){
+        xLoc = x;
+        yLoc = y;
+    }
+
+    public void paintComponent(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        // Settings.
+        g2d.setColor(Color.red);
+
+        // Draw point.
+        g2d.drawLine(xLoc, yLoc, xLoc, yLoc);
+
+    }
+
+}
---
+++ Raytracing3DEngine/src/com/github/MichielProost/Raytracing3DEngine/Raytracing3DEngine/Raytracing3DEngine.java
@@ -0,0 +1,12 @@
+package com.github.MichielProost.Raytracing3DEngine.Raytracing3DEngine;
+
+public class Raytracing3DEngine {
+    public static void main(String[] args) {
+
+        Test test = new Test();
+        test    .drawPoint()
+                .multiplyMatrices()
+                .addMatrices();
+
+    }
+}
