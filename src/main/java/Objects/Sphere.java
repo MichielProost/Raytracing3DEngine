3c2321ea4f990f1a0dba5a19a190e29f8005baa5
Michiel Proost <michiel.proost@gmail.com>
2021-01-05 16:06:40 +0100

    Sphere and Box are OK.

--- src/main/java/Objects/Sphere.java
+++ src/main/java/Objects/Sphere.java
@@ -1,6 +1,8 @@
 package Objects;
 
 import Matrix.*;
+import RayTracing.Intersection;
+import RayTracing.IntersectionMapHandler;
 import RayTracing.Ray;
 
 /**
@@ -9,44 +11,48 @@ import RayTracing.Ray;
 public class Sphere extends Shape {
 
     @Override
-    public Hit getIntersections(Ray ray) {
+    public Intersection getClosestIntersection(Ray ray) {
+        // Create intersection handler.
+        IntersectionMapHandler handler = new IntersectionMapHandler();
 
         // The starting point of the ray.
-        Vector origin = new Vector(ray.start.getX(), ray.start.getY(), ray.start.getZ());
+        Point origin = ray.start;
+
         // The direction of the ray.
         Vector direction = ray.dir;
 
+        // Calculate coefficients.
         double A = Math.pow( direction.getMagnitude() , 2 );
-        double B = origin.dot(direction);
+        double B = origin.dot( direction );
         double C = Math.pow( origin.getMagnitude(), 2 ) - 1;
 
+        // Calculate discriminant.
         double D = Math.pow(B, 2) - (A * C);
 
-        if(D < 0){
-            return null;
-        }
-
+        // Times at which the intersections occur.
         double t1 = (-B + Math.sqrt(D)) / (2 * A);
         double t2 = (-B - Math.sqrt(D)) / (2 * A);
 
-        if (t1 < 0 && t2 < 0)
+        // There are no intersections if the discriminant is smaller than zero.
+        if (D < 0){
             return null;
-        else if (t1 < 0)
-            return new Hit (
-                    t2,
-                    ray.getPoint(t2),
-                    new Vector( ray.getPoint( t2 ).getX(), ray.getPoint( t2 ).getY() , ray.getPoint( t2 ).getZ()));
-        else if (t2 < 0)
-            return new Hit (
-                    t1,
-                    ray.getPoint(t1),
-                    new Vector( ray.getPoint( t1 ).getX(), ray.getPoint( t1 ).getY() , ray.getPoint( t1 ).getZ()));
-        else {
-            double t = Math.min(t1, t2);
-            return new Hit (
-                    t,
-                    ray.getPoint(t),
-                    new Vector( ray.getPoint( t ).getX(), ray.getPoint( t ).getY() , ray.getPoint( t ).getZ()));
         }
+
+        // Add valid intersections.
+        if (t1 >= 0) {
+            Point location = ray.getPoint( t1 );
+            handler.addIntersection(
+                    new Intersection(t1, location, new Vector(location.getX(), location.getY(), location.getZ()))
+            );
+        }
+        if (t2 >= 0) {
+            Point location = ray.getPoint( t2 );
+            handler.addIntersection(
+                    new Intersection(t2, location, new Vector(location.getX(), location.getY(), location.getZ()))
+            );
+        }
+
+        return handler.getClosestIntersection();
     }
+
 }
