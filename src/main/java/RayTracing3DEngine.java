79905c7b9b0dbf506772631daaf966dcf6a63ab5
Michiel Proost <michiel.proost@gmail.com>
2021-01-23 17:16:33 +0100

    Very small update

--- src/main/java/RayTracing3DEngine.java
+++ src/main/java/RayTracing3DEngine.java
@@ -65,8 +65,8 @@ public class RayTracing3DEngine {
         IATFactory factory = new ATFactory();
 
         // Create a new scene.
-        //Scene scene = new EquilateralTriangle( maxRecursionLevel, 4, new Point(0, 0, 0));
-        Scene scene = new ShapesRow( maxRecursionLevel );
+        Scene scene = new EquilateralTriangle( maxRecursionLevel, 4, new Point(0, 0, 0));
+        //Scene scene = new ShapesRow( maxRecursionLevel );
 
         // Define light sources.
         LightSource source_left = new LightSource(2, 8, 0).setColor(new Rgb(0.7f, 0.7f, 0.7f));
